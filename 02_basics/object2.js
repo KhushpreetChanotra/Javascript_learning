a506b8f6d09adface41d85e7858249716abb08e7
KhushpreetChanotra <111775885+KhushpreetChanotra@users.noreply.github.com>
2024-02-15 02:26:36 +0000

    object2 depth

--- 02_basics/object2.js
+++ 02_basics/object2.js
@@ -10,7 +10,38 @@ d:4
 // merge two object 
 const obj1={a:1,b:2,c:3}
 const obj2={d:1,e:2,e:4}
-// const obj3={obj1,obj2} this is not the proper meathod 
+
+
+//const obj3=Object.assign({},obj1,obj2)
+// its an static method 
+// in the synatx of object.assign ({target},{source},.....,{nsource})
+const obj3={...obj1,...obj2}
 
 
 console.log(obj3)
+// another way of combining the object is spreading 
+
+
+/// isne ak synatax batya jab database se values aati hai
+
+
+// i am going to check the nested feature of object 
+
+const o={
+    a:{
+        b:{
+           h:"how are you hello world"
+        }
+    }
+}
+console.log(o.a.b.h)
+
+// object in an array which will come many times in real work
+const arr=[
+    {id:'75211',
+     name:'khushpreet '},
+     {id:'75211',
+     name:'khushpreet '}
+]
+console.log(arr[0].id)
+console.log(Object.keys(o))
