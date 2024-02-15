39d0b00a49bd54107da2df4492f8bcf49dce0f82
KhushpreetChanotra <111775885+KhushpreetChanotra@users.noreply.github.com>
2024-02-15 02:07:46 +0000

    object

---
+++ 02_basics/object2.js
@@ -0,0 +1,16 @@
+//const tinderuser=new Object() this type of decleartaion is called singleton 
+const tinderuser=
+{a:1,
+b:2,
+c:3,
+d:4
+} 
+//console.log(tinderuser)
+
+// merge two object 
+const obj1={a:1,b:2,c:3}
+const obj2={d:1,e:2,e:4}
+// const obj3={obj1,obj2} this is not the proper meathod 
+
+
+console.log(obj3)
