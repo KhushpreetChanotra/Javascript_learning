067a47860917527ab30584aa0f4b32ceac7452b3
KhushpreetChanotra <111775885+KhushpreetChanotra@users.noreply.github.com>
2024-02-18 05:18:39 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,18 @@
+// immediately Involked function Expression
+(function chai(){
+    console.log(`hello my name is khushpreet `)
+})();
+/// to end the code here we have to use ; in the end
+
+
+
+
+/////** remember one thing that fucntion chai ()() first parathesis is for function writting and second one is for execution*/
+
+
+
+
+
+///// important note for interview iife means jo fuction immmediatly execute ho jaye
+//// global scope k polluction se problem hoti hai kai baar so iske polluction ko haatane k liye yeh karte hai
+
